74cb22d4da8cacb511788e239684f031929a49ac
Tomasz Cieńciała <tomasz.cienciala@gmail.com>
2017-03-08 15:15:34 +0100

    seeds.rb fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ products = [
   ['Coffee Black', 3],
   ['Coffee White', 4],
   ['French Fries', 4],
-  ['Tortilla', 11],
+  ['Tortilla', 11]
 ]
 
-products.each{ |name, price| Product.create(name: name, price: price)}
+products.each { |name, price| Product.create(name: name, price: price) }
